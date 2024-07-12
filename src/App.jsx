219ed049aebf5caadb6c16e8c576fd2782ba1890
Stilinski <realesdarren@gmail.com>
2024-07-12 16:34:19 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <div>
+        <h1>Darren D. Reales</h1>
+        <h2>Jeffrey Toquero</h2>
+      </div>
+    </div>
+  )
+}
+
+export default App
